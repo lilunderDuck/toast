082dd54517a4e975100d5667c51f9b5b22270535
LazyyyDuck _ <thisisjustalongtext@gmail.com>
2024-12-01 21:52:10 +0700

    Changed the saved journal data location

--- src/server/internals/path.ts
+++ src/server/internals/path.ts
@@ -1,3 +1,9 @@
-export const DATA_FOLDER = __devMode ? './out/data' : './data'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export const DATA_FOLDER = __devMode ? './out/data' : `${__dirname}/data`
 export const JOURNALS_FOLDER = `${DATA_FOLDER}/journals` as const
 export const CACHE_FOLDER = `${DATA_FOLDER}/cache` as const
