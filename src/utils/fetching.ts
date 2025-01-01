7d90932e06a13c85f1e1ee226bce28fcb0a4c16b
LazyyyDuck _ <thisisjustalongtext@gmail.com>
2025-01-01 21:14:56 +0700

    `src/utils`: added documentation - Yeah, I did change the saved path in `src/server/internals/path.ts` too.

--- src/utils/fetching.ts
+++ src/utils/fetching.ts
@@ -1,18 +1,36 @@
 import { HttpMethod } from "~/common"
 
+/**The base path for making API requests. */
 const BASE_PATH = 'http://localhost:8000'
+
+/**Fetches data from a URL using the specified HTTP method.
+ * 
+ * When the data returned from the server, it will attempt to parse it as JSON and 
+ * return the parsed data. If the server responds with the status of `4xx` or `5xx`, this will return `null`.
+ * 
+ * @note This only work if the server return **only with JSON**.
+ * @template Data The type of the data expected in the response, defaults to an empty object (`{}`).
+ * @param method The HTTP method to use for the request (e.g., `'GET'`, `'POST'`, `'PATCH'`).
+ * @param path The path of the API endpoint relative to the base URL.
+ * @param body An optional object containing data to send in the request body (for `POST` and `PATCH` methods only).
+ * @returns A promise that resolves to the parsed response data as the specified 
+ * type (`Data`), or `null` if the request fails.
+ * @see {@link HttpMethod}
+ */
 export async function fetchIt<Data extends {} = {}>(method: HttpMethod, path: string, body = {}): Promise<Data | null> {
   console.groupCollapsed(`--> ${method}:`, BASE_PATH + path)
   const methodsThatAllowABody = ['POST', 'PATCH'].includes(method)
 
+  const dataWillBeSentToServer = {
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
   const response = await fetch(BASE_PATH + path, {
     method,
-    ...(methodsThatAllowABody ? {
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    } : {}),
+    ...(methodsThatAllowABody ? dataWillBeSentToServer : {}),
   })
 
   if (isServerResponsesWithBadStatusCode(response.status)) {
@@ -30,6 +48,11 @@ function isServerResponsesWithBadStatusCode(statusCode: number) {
   return statusCode >= 400
 }
 
+/**Attempts to parse a `Response` object as JSON and return the parsed data.
+ * @param response The `Response` object to parse.
+ * @returns A promise that resolves to the parsed JSON data as an object, or null if parsing fails.
+ * @see https://developer.mozilla.org/docs/Web/API/Response
+ */
 async function tryConvertingToJson(response: Response) {
   let json: {} | null = {}
   try {
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,9 +1,48 @@
 export interface IStorage<Mapping extends Record<string, any>> {
+  /**Retrieves the value associated with the given key.
+   * @param key The key to retrieve the value for.
+   * @returns The value associated with the key or `undefined` if the key does not exist.
+   */
   $get<T extends keyof Mapping>(key: T): Mapping[T]
+  /**Sets the value associated with the given key.
+   * @param key The key to set the value for.
+   * @param value The value to set for the key.
+   */
   $set<T extends keyof Mapping>(key: T, value: Mapping[T]): void
+  /**Deletes the value associated with the given key.
+   * @param key The key to delete.
+   */
   $delete<T extends keyof Mapping>(key: T): void
 }
 
+/**Create a type-safe version of `localStorage`, `sessionStorage`, ...
+ * 
+ * This function helps you interact with storage mechanisms like `localStorage` and `sessionStorage`
+ * in a type-safe way. It also automatically converts your data to strings for storage and back to
+ * their original types when retrieving data.
+ * 
+ * @example
+ * ```
+ * const storage = createStorage<{
+ *   something: number
+ * }>(localStorage)
+ * 
+ * storage.$set('something', 10)
+ * // no type error
+ * 
+ * storage.$set('something else', 10)
+ * //           ^^^^^^^^^^^^^^^^
+ * // Argument of type '"something else"' is not assignable to parameter of type '"something"'
+ * 
+ * storage.$set('something', 'invalid')
+ * //                        ^^^^^^^^^
+ * // Argument of type 'string' is not assignable to parameter of type 'number'
+ * ```
+ * 
+ * @param storage it can be `localStorage`, `sessionStorage`, or something else.
+ * @returns objects contain a few methods to interect with the browser storage.
+ * @see {@link IStorage}
+ */
 export function createStorage<Mapping extends Record<string, any>>(storage: Storage): IStorage<Mapping> {
   return {
     $get(key) {
