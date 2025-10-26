a58169e1ef74ed5fe8e4e41bbca08fd920d4e99e
unknown <thisisjustalongtext@gmail.com>
2025-10-26 19:32:51 +0700

    Refactor const enum script.

--- build/config/constEnum.ts
+++ build/config/constEnum.ts
@@ -1,9 +1,31 @@
 import { writeFileSync } from "node:fs"
 
-const defineMapping: Map<string, string> = new Map()
-const typeContent: Record<string, string[]> = {}
+type ValidEnum = Record<string, string | number>
 
-/**Simply just define a `const enum`. The enum types will be available in the global scope.
+function makeEnum() {
+  const mapping: { name: string, prop: ValidEnum }[] = []
+  return {
+    enumMapping: mapping,
+    defineEnum(name: string, prop: ValidEnum | string[]) {
+      let propMapping: ValidEnum = {}
+      if (Array.isArray(prop)) {
+        for (let i = 0; i < prop.length; i++) {
+          const propName = prop[i]
+          propMapping[propName] = i
+        }
+      } else {
+        propMapping = prop
+      }
+
+      mapping.push({
+        name,
+        prop: propMapping,
+      })
+    }
+  }
+}
+
+/**Simply just define all `const enum`s. The enum types will be available in the global scope.
  * 
  * This is a work-around to mimic `const enum` behavior. Because simply, `vite`, or more correctly, 
  * `esbuild`, doesn't support `const enum` to inline the value and stuff.
@@ -15,110 +37,82 @@ const typeContent: Record<string, string[]> = {}
  * @see https://github.com/vitest-dev/vitest/discussions/3964
  * @see https://github.com/evanw/esbuild/issues/2298#issuecomment-1146378367
  */
-function defineEnum(
-  enumName: string, 
-  props: Record<string, null | number | string>, 
-  moduleName: string = "global"
-) {
-  const entries = Object.entries(props)
-  let enumPropContent = ''
-
-  for (let i = 0; i < entries.length; i++) {
-    let [name, value] = entries[i]
-    if (!value) {
-      value = i
-    }
-
-    if (typeof value === "string") {
-      value = `"${value}"`
-    }
-
-    enumPropContent += `${name} = ${value},`
-    defineMapping.set(`${enumName}.${name}`, `${value}`)
-    console.log(`define: ${enumName}.${name} = `, value)
-  }
-
-  if (!typeContent[moduleName]) {
-    typeContent[moduleName] = []
-  }
-
-  typeContent[moduleName].push(`enum ${enumName} {${enumPropContent}}`)
-}
-
 export function defineAllEnum() {
-  defineEnum('ToastActionType', {
-    ADD_TOAST: null,
-    UPDATE_TOAST: null,
-    UPSERT_TOAST: null,
-    DISMISS_TOAST: null,
-    REMOVE_TOAST: null,
-    START_PAUSE: null,
-    END_PAUSE: null,
-  })
+  const { defineEnum, enumMapping } = makeEnum()
 
-  defineEnum('ToastType', {
-    SUCCESS: null,
-    ERROR: null,
-    LOADING: null,
-    BLANK: null,
-    CUSTOM: null
-  })
+  defineEnum('ToastActionType', [
+    'ADD_TOAST',
+    'UPDATE_TOAST',
+    'UPSERT_TOAST',
+    'DISMISS_TOAST',
+    'REMOVE_TOAST',
+    'START_PAUSE',
+    'END_PAUSE',
+  ])
 
-  defineEnum('ToastPosition', {
-    TOP_LEFT: null,
-    TOP_CENTER: null,
-    TOP_RIGHT: null,
-    BOTTOM_LEFT: null,
-    BOTTOM_CENTER: null,
-    BOTTOM_RIGHT: null
-  })
+  defineEnum('ToastType', [
+    'SUCCESS',
+    'ERROR',
+    'LOADING',
+    'BLANK',
+    'CUSTOM',
+  ])
 
-  defineEnum('ButtonVariant', {
-    DEFAULT: null,
-    DANGER: null,
-    OUTLINE: null,
-    SECONDARY: null,
-    GHOST: null,
-    LINK: null,
-  })
+  defineEnum('ToastPosition', [
+    'TOP_LEFT',
+    'TOP_CENTER',
+    'TOP_RIGHT',
+    'BOTTOM_LEFT',
+    'BOTTOM_CENTER',
+    'BOTTOM_RIGHT',
+  ])
 
-  defineEnum('ButtonSize', {
-    DEFAULT: null,
-    SMALL: null,
-    LARGE: null,
-    ICON: null,
-    UNSET: null,
-  })
+  defineEnum('ButtonVariant', [
+    'DEFAULT',
+    'DANGER',
+    'OUTLINE',
+    'SECONDARY',
+    'GHOST',
+    'LINK',
+  ])
 
-  defineEnum('FormSubmitState', {
-    SUCCESS: null,
-  })
+  defineEnum('ButtonSize', [
+    'DEFAULT',
+    'SMALL',
+    'LARGE',
+    'ICON',
+    'UNSET',
+  ])
 
-  defineEnum('SettingType', {
-    CUSTOM: null,
-    INPUT: null,
-    RANGE: null,
-    CHECKBOX: null,
-  })
+  defineEnum('FormSubmitState', [
+    'SUCCESS',
+  ])
 
-  defineEnum('VideoLoadingStatus', {
-    START_LOADING: null,
-    FINISH_LOADING: null,
-    ERROR: null
-  })
+  defineEnum('SettingType', [
+    'CUSTOM',
+    'INPUT',
+    'RANGE',
+    'CHECKBOX',
+  ])
 
-  defineEnum('ButtonRowDirection', {
-    LEFT: null,
-    RIGHT: null,
-    MIDDLE: null,
-    CUSTOM: null,
-  })
+  defineEnum('VideoLoadingStatus', [
+    'START_LOADING',
+    'FINISH_LOADING',
+    'ERROR',
+  ])
 
-  defineEnum('FileUploadType', {
-    DIRECTORY: null,
-    FILE: null,
-    MULTI_FILE: null
-  })
+  defineEnum('ButtonRowDirection', [
+    'LEFT',
+    'RIGHT',
+    'MIDDLE',
+    'CUSTOM',
+  ])
+
+  defineEnum('FileUploadType', [
+    'DIRECTORY',
+    'FILE',
+    'MULTI_FILE',
+  ])
 
   // 1 to 1 mapping from ./backend/features/editor/utils.go
   defineEnum('FileType', {
@@ -136,69 +130,77 @@ export function defineAllEnum() {
     LEFT_HAND_TAPPED: "dc"
   })
 
-  defineEnum('GalleryViewMode', {
-    SINGLE_ITEM: null,
-    SPLIT_VIEW: null,
-  })
+  defineEnum('GalleryViewMode', [
+    'SINGLE_ITEM',
+    'SPLIT_VIEW',
+  ])
 
-  defineEnum('CurrentlyOpenedHeaderAction', {
-    TOGGLE_SIDEBAR: null,
-    GO_BACK_TO_HOME: null
-  })
+  defineEnum('CurrentlyOpenedHeaderAction', [
+    'TOGGLE_SIDEBAR',
+    'GO_BACK_TO_HOME',
+  ])
 
-  defineEnum('EditorEvent', {
-    ON_SWITCHING: null,
-    ON_UPDATE: null,
-    UPDATE_BONGO_CAT_ANIMATION: null
-  })
+  defineEnum('EditorEvent', [
+    'ON_SWITCHING',
+    'ON_UPDATE',
+    'UPDATE_BONGO_CAT_ANIMATION',
+  ])
 
-  defineEnum('JournalType', {
-    JOURNAL: null,
-    FOLDER: null
-  })
+  defineEnum('JournalType', [
+    'JOURNAL',
+    'FOLDER',
+  ])
 
-  defineEnum('PluginEvent', {
-    // internals
-    REGISTER_EDITOR_NODE: null,
-    // loading,
-    JOURNAL_LOADED: null
-  })
+  defineEnum('PluginEvent', [
+    'REGISTER_EDITOR_NODE',
+    'JOURNAL_LOADED',
+  ])
 
-  defineEnum('MediaState', {
-    LOADING: null,
-    FINISHED_LOADING: null,
-    PLAYING: null,
-    PAUSED: null,
-    ERROR: null,
-    COMPLETED: null
-  })
+  defineEnum('MediaState', [
+    'LOADING',
+    'FINISHED_LOADING',
+    'PLAYING',
+    'PAUSED',
+    'ERROR',
+    'COMPLETED',
+  ])
 
-  defineEnum('MediaEvent', {
-    STATE_UPDATE: null
-  })
+  defineEnum('MediaEvent', [
+    'STATE_UPDATE',
+  ])
 
-  defineEnum('PlaylistButtonRowAction', {
-    NEXT_TRACK: null,
-    PREVIOUS_TRACK: null,
-    TOGGLE_PLAY_TRACK: null
-  })
+  defineEnum('PlaylistButtonRowAction', [
+    'NEXT_TRACK',
+    'PREVIOUS_TRACK',
+    'TOGGLE_PLAY_TRACK',
+  ])
+
+  const enumDefinedMapping = new Map<string, string | number>()
 
   return {
     generateType() {
       let content = "// this file is auto-generated when on dev/build mode\nexport {};"
-      for (const [moduleName, moduleContent] of Object.entries(typeContent)) {
-        if (moduleName === "global") {
-          content += `declare global { ${moduleContent.join('')} }`
-          continue
+      for (const definedEnum of enumMapping) {
+        const entries = Object.entries(definedEnum.prop)
+        let enumContent = ''
+        for (let [enumPropName, enumPropValue] of entries) {
+          enumContent += `${enumPropName} = ${enumPropValue},`
+          if (typeof enumPropValue === "string") {
+            enumPropValue = `"${enumPropValue}"`
+          }
+
+          const fullName = `${definedEnum.name}.${enumPropName}` as const
+          enumDefinedMapping.set(fullName, enumPropValue)
+          console.log(`Define:\t\t${fullName} = ${enumPropValue}`)
         }
 
-        content += `declare module ${moduleName} { ${moduleContent.join('')} }`
+        content += `enum ${definedEnum.name} {${enumContent}}`
       }
 
       writeFileSync("./build/dist/enum_generated.d.ts", content)
     },
     getDefineList() {
-      return Object.fromEntries(defineMapping.entries())
+      return Object.fromEntries(enumDefinedMapping.entries())
     }
   }
 }
